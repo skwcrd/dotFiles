d1a42c3fb401efb7acd6f3454cc3b9ae8fd60a74
SkWcRd <32414976+skwcrd@users.noreply.github.com>
2019-09-10 09:58:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,20 +49,26 @@ sudo apt-get install -y build-essential
 clear
 
 #install react js
-echo " install === NodeJS ===> ReactJS"
+echo " install === NodeJS ===> Packege"
 npm install -g create-react-app
+npm install -g express-generator
+npm install -g @angular/cli
+npm install -g nodemon
 clear
 
 #install visual studio code
 echo " install === EDITOR ===> Visual Studio Code"
-sudo dpkg -i code_1.21.1-1521038896_amd64.deb
-#Install dependencies
-sudo apt-get install -f
+sudo apt install ./<file>.deb
+# If you're on an older Linux distribution, you will need to run this instead:
+# sudo dpkg -i <file>.deb
+# Install dependencies
+# sudo apt-get install -f
 #The repository and key
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 #Then update the package cache and install the package
+sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install code
 clear
@@ -81,14 +87,14 @@ echo " install ===> Others..."
 #while read -r list; do sudo apt-get install -y "$list"; done < packages.txt
 sudo apt-get install -y network-manager-pptp network-manager-pptp-gnome default-jdk default-jre gcc g++ golang golang-go libunwind8 gettext apt-transport-https metasploit-framework postgresql
 sudo apt-get install -y libevent-dev libopenblas-dev libatlas3gf-base libfreetype6-dev
-sudo apt-get install -y python python-numpy python-scipy python-dev python-pip python-nose
-sudo apt-get install -y python3 python3-numpy python3-scipy python3-dev python3-pip python3-nose
+#sudo apt-get install -y python python-numpy python-scipy python-dev python-pip python-nose
+#sudo apt-get install -y python3 python3-numpy python3-scipy python3-dev python3-pip python3-nose
 clear
 
-sudo apt-get install -y python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil
-sudo pip3 install pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd
-sudo pip install -r requirements.txt
-sudo pip3 install -r requirements.txt
+#sudo apt-get install -y python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil
+#sudo pip3 install pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd
+#sudo pip install -r requirements.txt
+#sudo pip3 install -r requirements.txt
 
 #https://github.com/Theano/Theano
 #sudo pip install Theano
@@ -118,5 +124,6 @@ EOF
 #clean and clear
 echo " ===> Clear"
 sudo apt-get -y autoremove
+sudo apt-get -y autoclean
 sudo apt-get clean
 clear
